3b664af1335235b46d7d0ee0edc0fe3fb8c9f634
navid <vahidi.navid0017@gmail.com>
2023-03-14 00:00:36 +0330

    The fourth step: creating the mine method and creating an object

--- src/firstproject/DemoTurners.java
+++ src/firstproject/DemoTurners.java
@@ -1,4 +1,15 @@
 package firstproject;
 
+import java.util.Objects;
+
 public class DemoTurners {
+    public static void main(String[] args) {
+        Leaf leaf=new Leaf();
+        Page page=new Page();
+        Pancake pancake=new Pancake();
+        leaf.turn();
+        page.turn();
+        pancake.turn();
+        
+    }
 }
