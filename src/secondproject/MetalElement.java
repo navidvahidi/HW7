e293dadc490e605bf899c15fbc2d980703eb0d0f
navid <vahidi.navid0017@gmail.com>
2023-03-15 14:14:40 +0330

    second exercise - Step 4: Complete the describeElement method

--- src/secondproject/MetalElement.java
+++ src/secondproject/MetalElement.java
@@ -1,13 +1,12 @@
 package secondproject;
 
-public class MetalElement extends Element{
+public class MetalElement extends Element {
 
     public MetalElement(String symbol, int atomicNumber, double atomicWeight) {
         super(symbol, atomicNumber, atomicWeight);
     }
-
     @Override
     public void describeElement() {
-
+        System.out.println("Symbol :" + getSymbol() + "   Atomic Number :" + getAtomicNumber() + "     Atomic Weight  :" + getAtomicWeight() + "   >>> A metal that conducts electricity ");
     }
 }
