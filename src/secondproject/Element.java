5e136cb31e3189e1cbbb549c9b90167a4a7bd171
navid <vahidi.navid0017@gmail.com>
2023-03-15 14:08:17 +0330

    Second exercise - Step 2: Add variable and method to Element class

--- src/secondproject/Element.java
+++ src/secondproject/Element.java
@@ -1,5 +1,27 @@
 package secondproject;
 
-public class Element {
+public abstract class Element {
+    String symbol;
+    int atomicNumber;
+    double atomicWeight;
 
+    public Element(String symbol, int atomicNumber, double atomicWeight) {
+        this.symbol = symbol;
+        this.atomicNumber = atomicNumber;
+        this.atomicWeight = atomicWeight;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getAtomicNumber() {
+        return atomicNumber;
+    }
+
+    public double getAtomicWeight() {
+        return atomicWeight;
+    }
+
+    public abstract void describeElement();
 }
