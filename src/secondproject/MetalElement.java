c21e527b55cabeb94ef3e1962b98ca456ce6b40f
navid <vahidi.navid0017@gmail.com>
2023-03-15 14:11:05 +0330

    second exercise - Step 3: Creating the Metal Element class and extending the Element class

---
+++ src/secondproject/MetalElement.java
@@ -0,0 +1,13 @@
+package secondproject;
+
+public class MetalElement extends Element{
+
+    public MetalElement(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
+
+    @Override
+    public void describeElement() {
+
+    }
+}
