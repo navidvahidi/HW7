4591a5f9f94adbc4ffbbdbdefbdce0ead9ca15c0
navid <vahidi.navid0017@gmail.com>
2023-03-15 14:23:09 +0330

    second exercise - Step 5 : Creating the NonMetal Element class and extending the Element class

---
+++ src/secondproject/NonMetalElement.java
@@ -0,0 +1,13 @@
+package secondproject;
+
+public class NonMetalElement extends Element{
+
+    public NonMetalElement(String symbol, int atomicNumber, double atomicWeight) {
+        super(symbol, atomicNumber, atomicWeight);
+    }
+
+    @Override
+    public void describeElement() {
+
+    }
+}
