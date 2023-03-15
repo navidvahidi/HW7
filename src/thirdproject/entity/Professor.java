3714e9603422d9cb48817b01c1298db4a9748ab5
navid <vahidi.navid0017@gmail.com>
2023-03-16 02:25:47 +0330

    The third exercise - Step 3: Creating the Professor class and extends it from the UniversityEntty class

---
+++ src/thirdproject/entity/Professor.java
@@ -0,0 +1,10 @@
+package thirdproject.entity;
+
+public class Professor extends UniversityEntty {
+    public Professor() {
+    }
+
+    public Professor(int id, String name) {
+        super(id, name);
+    }
+}
